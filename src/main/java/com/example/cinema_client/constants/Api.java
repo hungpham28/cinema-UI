8d68ac0926cd4a9103a5fc09e38fad4c7b6ec6eb
hungpham28 <88095612+hungpham28@users.noreply.github.com>
2022-09-09 23:08:47 +0700

    Update Api.java

--- src/main/java/com/example/cinema_client/constants/Api.java
+++ src/main/java/com/example/cinema_client/constants/Api.java
@@ -1,6 +1,6 @@
 package com.example.cinema_client.constants;
 
 public class Api {
-
-    public static String baseURL="http://localhost:8080";
+    public static String baseURL="https://ap-movie-cinema.herokuapp.com";
+//     public static String baseURL="http://localhost:8080";
 }
